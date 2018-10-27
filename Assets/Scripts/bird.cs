538be5ededfcb1fbc7a29e40d413275a110a2805
KTKevin <ktkim@ucsd.edu>
2018-10-26 21:52:30 -0700

    got sidescrolling completed need to work on animation

---
+++ Assets/Scripts/bird.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    public float upForce = 200f;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+
+            {
+                rb2d.position = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+
+
+
+                //anim.SetTrigger("Flap");
+            }
+           
+        }
+		
+	}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+        gameControl.instance.birdDied();
+        
+    }
+}
---
+++ Assets/Scripts/gameControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class gameControl : MonoBehaviour {
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public static gameControl instance;
+    public float scrollSpeed = -1.5f;
+   
+    // Use this for initialization
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start () {
+        gameOverText.SetActive(false);
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (gameOver && Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+		
+	}
+    public void birdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
